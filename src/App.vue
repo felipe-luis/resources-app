316b44b59fe00552d5c5cf140484ed29310d4e8d
felipe-luis <felipellopes12@hotmail.com>
2024-12-14 12:08:25 -0300

    Configuração inicial do projeto e criação do componente LearningResource

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<ul>
+		<LearningResource v-for="resource in storedResources" :key="resource"
+							:title="resource.title"
+							:description="resource.description"
+							:link="resource.link"></LearningResource>
+	</ul>
+</template>
+
+<script>
+import LearningResource from './components/LearningResources/LearningResource.vue';
+export default{
+	components:{
+		LearningResource
+	},
+	data(){
+		return{
+			storedResources: [
+				{
+					id: 'official-guide',
+					title: 'Official Guide',
+					description: 'The official Vue.js documentation',
+					link: 'https://vuejs.org'
+				},
+				{
+					id: 'google',
+					title: 'Google',
+					description: 'Learn to Google',
+					link: 'https://google.com'
+				}
+			]
+		}
+	}
+}
+</script>
