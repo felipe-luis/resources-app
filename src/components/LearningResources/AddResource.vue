5152e1d48564b0e868b135d2332c03c640ecef85
felipe-luis <felipellopes12@hotmail.com>
2024-12-14 19:47:39 -0300

    Adiciona formulário no AddResource

--- src/components/LearningResources/AddResource.vue
+++ src/components/LearningResources/AddResource.vue
@@ -1,3 +1,64 @@
 <template>
-    <h2>Add Resource</h2>
+    <BaseCard>
+        <form>
+            <div class="form-control">
+                <label for="title">Title</label>
+                <input id="title" name="title" type="text" v-model="title">
+            </div>
+            <div class="form-control">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" rows="3" v-model="description"></textarea>
+            </div>
+
+            <div class="form-control">
+                <label for="link">Link</label>
+                <input id="link" name="link" type="url" v-model="link">
+            </div>
+            <div>
+                <BaseButton type="submit">Add Resource</BaseButton>
+            </div>
+        </form>
+    </BaseCard>
 </template>
+
+<script>
+export default{
+    data(){
+        return{
+            title:'',
+            description:'',
+            link: ''
+        }
+    }
+}
+
+</script>
+<style scoped>
+label{
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
+
+input, 
+textarea{
+    display: block;
+    width: 100%;
+    font: inherit;
+
+    padding: 0.15rem;
+    border: 1px solid #ccc;
+
+}
+
+input:focus,
+textarea:focus{
+    outline: none;
+    border-color: #3a0061;
+    background-color: #f7ebff;
+}
+
+.form-control{
+    margin: 1rem 0;
+}
+</style>
